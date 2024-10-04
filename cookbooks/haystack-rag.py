8a01640df6b65539095f69dcbdcdd26dc613b741
Mike Brinker <themikejr@gmail.com>
2024-10-04 10:24:09 -0400

    First haystack RAG tutorial.

---
+++ cookbooks/haystack-rag.py
@@ -0,0 +1,92 @@
+# From Haystack tutorial:
+# https://haystack.deepset.ai/tutorials/27_first_rag_pipeline
+
+import os
+from getpass import getpass
+from dotenv import load_dotenv
+
+from haystack.telemetry import tutorial_running
+from haystack.document_stores.in_memory import InMemoryDocumentStore
+from haystack.components.embedders import SentenceTransformersDocumentEmbedder
+from haystack.components.embedders import SentenceTransformersTextEmbedder
+from haystack.components.retrievers.in_memory import InMemoryEmbeddingRetriever
+from haystack.components.builders import PromptBuilder
+from haystack.components.generators import OpenAIGenerator
+
+from haystack import Document
+from haystack import Pipeline
+
+# Load environment variables
+load_dotenv()
+
+doc_embedder = SentenceTransformersDocumentEmbedder(
+    model="sentence-transformers/all-MiniLM-L6-v2"
+)
+doc_embedder.warm_up()
+
+from datasets import load_dataset
+
+tutorial_running(27)
+
+print("Creating document store...")
+document_store = InMemoryDocumentStore()
+
+print("Loading dataset")
+dataset = load_dataset("bilgeyucel/seven-wonders", split="train")
+docs = [Document(content=doc["content"], meta=doc["meta"]) for doc in dataset]
+
+print("Loading embedder")
+doc_embedder = SentenceTransformersDocumentEmbedder(
+    model="sentence-transformers/all-MiniLM-L6-v2"
+)
+doc_embedder.warm_up()
+
+print("Generating embeddings")
+docs_with_embeddings = doc_embedder.run(docs)
+document_store.write_documents(docs_with_embeddings["documents"])
+
+text_embedder = SentenceTransformersTextEmbedder(
+    model="sentence-transformers/all-MiniLM-L6-v2"
+)
+
+retriever = InMemoryEmbeddingRetriever(document_store)
+
+template = """
+Given the following information, answer the question.
+
+Context:
+{% for document in documents %}
+    {{ document.content }}
+{% endfor %}
+
+Question: {{question}}
+Answer:
+"""
+
+prompt_builder = PromptBuilder(template=template)
+
+if "OPENAI_API_KEY" not in os.environ:
+    os.environ["OPENAI_API_KEY"] = getpass("Enter OpenAI API key:")
+generator = OpenAIGenerator(model="gpt-4o-mini")
+
+basic_rag_pipeline = Pipeline()
+
+# Add components to your pipeline
+basic_rag_pipeline.add_component("text_embedder", text_embedder)
+basic_rag_pipeline.add_component("retriever", retriever)
+basic_rag_pipeline.add_component("prompt_builder", prompt_builder)
+basic_rag_pipeline.add_component("llm", generator)
+
+# Now, connect the components to each other
+basic_rag_pipeline.connect("text_embedder.embedding", "retriever.query_embedding")
+basic_rag_pipeline.connect("retriever", "prompt_builder.documents")
+basic_rag_pipeline.connect("prompt_builder", "llm")
+
+
+question = "Where are the Gardens of Babylon?"
+
+response = basic_rag_pipeline.run(
+    {"text_embedder": {"text": question}, "prompt_builder": {"question": question}}
+)
+
+print(response["llm"]["replies"][0])
